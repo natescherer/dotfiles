eababbbf434d0e3c5ec3cff3ef4dde41467ac35d
Nate Scherer <376408+natescherer@users.noreply.github.com>
2025-04-01 23:28:28 +0000

    fix: fix conflict checker for linux/macos

--- src/.chezmoiscripts/linux_and_macos/run_confict_checker.sh
+++ src/.chezmoiscripts/linux_and_macos/run_confict_checker.sh
@@ -9,7 +9,7 @@ declare -a badpaths=(
 )
 
 for path in "${badpaths[@]}"; do
-    if test -f $path; then
+    if test -f "`eval echo $path`"; then 
         echo "A file was found at '$path' which might conflict with chezmoi config. Please review and delete this file!"
     fi
 done
