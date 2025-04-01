7a8a7e06342a61fd3ec3b80e24649cb4a16e866d
Nate Scherer <376408+natescherer@users.noreply.github.com>
2025-04-01 23:30:32 +0000

    feat: add color to linux/macos conflict checker

--- src/.chezmoiscripts/linux_and_macos/run_confict_checker.sh
+++ src/.chezmoiscripts/linux_and_macos/run_confict_checker.sh
@@ -10,6 +10,6 @@ declare -a badpaths=(
 
 for path in "${badpaths[@]}"; do
     if test -f "`eval echo $path`"; then 
-        echo "A file was found at '$path' which might conflict with chezmoi config. Please review and delete this file!"
+        echo -e "\e[31mA file was found at '$path' which might conflict with chezmoi config. Please review and delete this file!\e[0m"
     fi
 done
