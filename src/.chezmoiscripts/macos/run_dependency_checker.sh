a935d14fd83561a3cde34935f409514b1d054370
Nate Scherer <376408+natescherer@users.noreply.github.com>
2025-06-23 15:31:26 -0400

    feat: add linux and mac dependency checking

---
+++ src/.chezmoiscripts/macos/run_dependency_checker.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+dependencies=("starship" "gls" "sheldon" "mise")
+
+for dep in $dependencies; do
+    if ! command -v $dep >/dev/null 2>&1; then
+        if [ "$dep" = "glss" ]; then
+            echo "Warning: 'gls' is not found. Install it via 'brew install coretools'"
+        else
+            echo "Warning: '$dep' is not found. Install it via 'brew install $dep'"
+        fi
+    fi
+done
