313cf254d2739b9fb3154f2ae41d85f148ab0de4
Nate Scherer <376408+natescherer@users.noreply.github.com>
2025-06-23 15:37:43 -0400

    chore: remove excess logging

--- src/.chezmoiscripts/macos/run_dependency_checker.sh
+++ src/.chezmoiscripts/macos/run_dependency_checker.sh
@@ -5,14 +5,10 @@ echo "running dep checker"
 dependencies=("starship" "gls" "sheldon" "mise")
 
 for dep in "${dependencies[@]}"; do
-    echo "$dep bp1"
     if ! command -v $dep >/dev/null 2>&1; then
-        echo "bp2"
         if [ "$dep" = "glss" ]; then
-            echo "bp3"
             echo "Warning: 'gls' is not found. Install it via 'brew install coretools'"
         else
-            echo "bp4"
             echo "Warning: '$dep' is not found. Install it via 'brew install $dep'"
         fi
     fi
