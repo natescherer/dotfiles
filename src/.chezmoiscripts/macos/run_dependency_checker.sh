16b0a7a6cc54cd02e43d3a4821ed42c5db959782
Nate Scherer <376408+natescherer@users.noreply.github.com>
2025-06-23 15:38:59 -0400

    feat: colorize dependency check output

--- src/.chezmoiscripts/macos/run_dependency_checker.sh
+++ src/.chezmoiscripts/macos/run_dependency_checker.sh
@@ -7,9 +7,9 @@ dependencies=("starship" "gls" "sheldon" "mise")
 for dep in "${dependencies[@]}"; do
     if ! command -v $dep >/dev/null 2>&1; then
         if [ "$dep" = "glss" ]; then
-            echo "Warning: 'gls' is not found. Install it via 'brew install coretools'"
+            echo "\033[1;33mWarning: 'gls' is not found. Install it via 'brew install coretools'\033[0m"
         else
-            echo "Warning: '$dep' is not found. Install it via 'brew install $dep'"
+            echo "\033[1;33mWarning: '$dep' is not found. Install it via 'brew install $dep'\033[0m"
         fi
     fi
 done
