562cfa853e605ef862453d9ba6bd278dcdcbd2f9
Nate Scherer <376408+natescherer@users.noreply.github.com>
2025-06-23 13:58:46 -0400

    fix: fix defaults script

--- src/.chezmoiscripts/macos/run_onchange_defaults-importer.sh
+++ src/.chezmoiscripts/macos/run_onchange_defaults-importer.sh
@@ -2,7 +2,7 @@
 
 # The below ensures that this file will change when the hashed files change, triggering this script to execute
 {{ range .macos_defaults_apps -}}
-## {{ . }} hash: {{ include ".scriptdata/macos_defaults/{{ . }}" | sha256sum }}
+## {{ . }} hash: {{ include . | sha256sum }}
 {{ end -}}
 
 domains=({{ .macos_defaults_apps | quoteList | join " " }})
