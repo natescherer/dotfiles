06c1f5e499c0115e4ec09f5860c4cd2550e7bfce
Nate Scherer <376408+natescherer@users.noreply.github.com>
2025-06-23 15:34:34 -0400

    feat: more logging

--- src/.chezmoiscripts/macos/run_dependency_checker.sh
+++ src/.chezmoiscripts/macos/run_dependency_checker.sh
@@ -5,10 +5,14 @@ echo "running dep checker"
 dependencies=("starship" "gls" "sheldon" "mise")
 
 for dep in $dependencies; do
+    echo "bp1"
     if ! command -v $dep >/dev/null 2>&1; then
+        echo "$dep bp2"
         if [ "$dep" = "glss" ]; then
+            echo "bp3"
             echo "Warning: 'gls' is not found. Install it via 'brew install coretools'"
         else
+            echo "bp4"
             echo "Warning: '$dep' is not found. Install it via 'brew install $dep'"
         fi
     fi
