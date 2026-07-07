053e40a97410808e1f9de50f1124e07b8ab7d16f
Nate Scherer <376408+natescherer@users.noreply.github.com>
2026-07-06 20:34:33 -0400

    feat: update conflict checker for coreutils
    Signed-off-by: Nate Scherer <376408+natescherer@users.noreply.github.com>

--- src/.chezmoiscripts/windows/run_conflict_checker.ps1
+++ src/.chezmoiscripts/windows/run_conflict_checker.ps1
@@ -1,7 +1,6 @@
 $BadPaths = @(
     "$PSHOME\Profile.ps1",
     "$PSHOME\Microsoft.PowerShell_profile.ps1",
-    "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1",
     "$env:USERPROFILE\.gitconfig",
     "$env:USERPROFILE\.nanorc"
 )
@@ -10,4 +9,11 @@ foreach ($Path in $BadPaths) {
     if (Test-Path $Path) {
         Write-Host -Object "A file was found at '$Path' which might conflict with chezmoi config. Please review and delete this file!" -ForegroundColor Red
     }
+}
+
+# This is a special check for coreutils
+$CoreUtilsCheckPath = "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+
+if (((Get-Content -Path $CoreUtilsCheckPath)[1] -notlike '*DO NOT MODIFY -- coreutils*') -and ((Get-Content -Path $CoreUtilsCheckPath)[-1] -notlike '*DO NOT MODIFY -- coreutils*')) {
+    Write-Output "File at '$Path' appears to contain more than just coreutils config. Remove anything not coreutils-related."
 }
