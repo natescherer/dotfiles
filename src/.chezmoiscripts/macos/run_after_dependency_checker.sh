a8af02cc86092e18d3ed94d7c11e29edd22889a3
Nate Scherer <376408+natescherer@users.noreply.github.com>
2026-07-29 22:28:40 -0400

    fix: fix typos
    Signed-off-by: Nate Scherer <376408+natescherer@users.noreply.github.com>

--- src/.chezmoiscripts/macos/run_after_dependency_checker.sh
+++ src/.chezmoiscripts/macos/run_after_dependency_checker.sh
@@ -4,7 +4,7 @@ dependencies=("starship" "fzf" "gls" "sheldon" "mise")
 
 for dep in "${dependencies[@]}"; do
     if ! command -v $dep >/dev/null 2>&1; then
-        if [ "$dep" = "glss" ]; then
+        if [ "$dep" = "gls" ]; then
             echo -e "\033[1;33mWarning: 'gls' is not found. Install it via 'brew install coretools'\033[0m"
         else
             echo -e "\033[1;33mWarning: '$dep' is not found. Install it via 'brew install $dep'\033[0m"
