b4d53d21c76137e7a3ea5f68a0d7052d42047a8f
Nate Scherer <376408+natescherer@users.noreply.github.com>
2026-08-01 17:12:08 -0400

    feat: start windows init scripts
    Signed-off-by: Nate Scherer <376408+natescherer@users.noreply.github.com>

---
+++ init-windows1.ps1
@@ -0,0 +1,15 @@
+#Requires -Version 5.1
+
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if (-not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    throw "This script must be run as Administrator."
+}
+
+Write-Host "`nEnsuring Microsoft.AppInstaller is up-to-date..." -ForegroundColor BrightGreen
+
+winget settings --enable BypassCertificatePinningForMicrosoftStore
+winget upgrade Microsoft.AppInstaller --accept-source-agreements --accept-package-agreements
+winget settings --disable BypassCertificatePinningForMicrosoftStore
+winget source reset --force
+
+Write-Host "`nMicrosoft.AppInstaller updated! Continue with  init-windows2.ps1 in a non-admin Terminal." -ForegroundColor BrightGreen
